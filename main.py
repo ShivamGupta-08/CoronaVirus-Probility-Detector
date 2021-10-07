2674b4730f975a5543e3161de9622981fd25c5eb
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2021-10-07 10:31:28 +0530

    CoronaVirus Probility Detector 1.1

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 file  = open('model.pkl','rb')
 clf = pickle.load(file)
 file.close()
+
+@app.route('/about/')
+def about():
+    return render_template('about us.html')
 @app.route("/" , methods=['GET','POST'])
 def hello_world():
     if request.method == "POST":
@@ -17,9 +21,8 @@ def hello_world():
         
         input_features = [fever,bodyPain,age,runnyNose,diffBreath]
         inf_prob=clf.predict_proba([input_features])[0][1]
-        
         print(inf_prob*100)
-        return render_template('show.html',inf=round( inf_prob*100,2))    
+        return render_template('show.html',inf=round( inf_prob*100,2))   
     return render_template('index.html') 
     # return "Hello, World!" +str(inf_prob)
 
